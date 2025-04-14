7c500b48f13c0bd06ca805927a6ceeb0a4e6f9df
datleXxx <129921351+datleXx@users.noreply.github.com>
2025-04-15 01:05:36 +1000

    alter migration file

--- drizzle/0006_gray_unicorn.sql
+++ drizzle/0006_gray_unicorn.sql
@@ -1,6 +1,6 @@
-ALTER TABLE "feed" ALTER COLUMN "id" SET DATA TYPE uuid;--> statement-breakpoint
+ALTER TABLE "feed" ALTER COLUMN "id" SET DATA TYPE uuid USING id::uuid;--> statement-breakpoint
 ALTER TABLE "feed" ALTER COLUMN "id" SET DEFAULT gen_random_uuid();--> statement-breakpoint
-ALTER TABLE "organization_to_feed" ALTER COLUMN "id" SET DATA TYPE uuid;--> statement-breakpoint
+ALTER TABLE "organization_to_feed" ALTER COLUMN "id" SET DATA TYPE uuid USING id::uuid;--> statement-breakpoint
 ALTER TABLE "organization_to_feed" ALTER COLUMN "id" SET DEFAULT gen_random_uuid();--> statement-breakpoint
-ALTER TABLE "organization" ALTER COLUMN "id" SET DATA TYPE uuid;--> statement-breakpoint
+ALTER TABLE "organization" ALTER COLUMN "id" SET DATA TYPE uuid USING id::uuid;--> statement-breakpoint
 ALTER TABLE "organization" ALTER COLUMN "id" SET DEFAULT gen_random_uuid();
