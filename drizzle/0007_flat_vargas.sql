321c0feffc02bc02a5ee2e2bb8ae676c57eec79d
datleXxx <129921351+datleXx@users.noreply.github.com>
2025-04-15 00:59:28 +1000

    Add drizzle migration files

---
+++ drizzle/0007_flat_vargas.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "organization_to_feed" ALTER COLUMN "feed_id" SET DATA TYPE uuid;--> statement-breakpoint
+ALTER TABLE "organization_to_feed" ALTER COLUMN "organization_id" SET DATA TYPE uuid;
